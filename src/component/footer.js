27664afb63bd954925136843b9385cd92bc04485
SeLFsTeR <p.a.meinema@gmail.com>
2019-10-21 17:51:40 +0200

    footer added

---
+++ src/component/footer.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class footer extends Component {
+  render() {
+    return (
+      <footer className="Foot">
+        This is the combined work of Lydia and Paul
+      </footer>
+    );
+  }
+}
