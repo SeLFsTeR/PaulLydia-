8a503378b4c1248b882fc420570828c0f7cf390e
Lydia-coder <lydiatjuh@gmail.com>
2019-10-21 17:19:13 +0200

    title change

---
+++ src/component/title.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+export default class Title extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Our MEME Page</h1>
+        <h2>hello</h2>
+      </div>
+    );
+  }
+}
