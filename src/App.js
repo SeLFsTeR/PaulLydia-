c83a9c3b16b3b5c8db5b2beb6e48955b61bc39a9
Lydia-coder <lydiatjuh@gmail.com>
2019-10-21 17:32:35 +0200

    change

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
@@ -12,30 +11,4 @@ export default class App extends React.Component {
       </div>
     );
   }
-=======
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import header from './components/header'
- 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          <code>src/App.js</code>
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          LOL
-        </a>
-      </header>
-    </div>
-  );
->>>>>>> 18c4840a22dabde4fa10c9f245436a20e5e9230b
 }
