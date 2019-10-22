d20a5d286d6a7b7e8ed356afd90bc6dd6429ab61
SeLFsTeR <p.a.meinema@gmail.com>
2019-10-22 15:44:43 +0200

    commit

--- src/component/footer.js
+++ src/component/footer.js
@@ -1,11 +1,1 @@
 import React, { Component } from "react";
-
-export default class footer extends Component {
-  render() {
-    return (
-      <footer className="Foot">
-        This is the combined work of Lydia and Paul
-      </footer>
-    );
-  }
-}
