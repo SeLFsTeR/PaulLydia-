4aed0271200f94fc7d766865b3b9c43b89429cef
SeLFsTeR <p.a.meinema@gmail.com>
2019-10-22 15:50:41 +0200

    commit

--- src/component/footer.js
+++ src/component/footer.js
@@ -1,1 +1,14 @@
-import React, { Component } from "react";
+import React from "react";
+
+const Footer = ({ title }) => <footer>{title}</footer>;
+
+class App extends React.Component {
+  render() {
+    const {footer} = this.props;
+    return (
+      
+        <Footer title={footer}/>
+      </div>
+    );
+  }
+};
