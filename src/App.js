80d44dbeb7a36626cdc2d0ecb63c832532468644
SeLFsTeR <p.a.meinema@gmail.com>
2019-10-23 16:36:24 +0200

    Added like button

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React from "react";
 import "./App.css";
 import Title from "./component/Title";
 import Footer from "./component/Footer";
+import Likebutton from "./component/Likebutton";
 
 export default class App extends React.Component {
   render() {
     return (
       <div className="App">
         <Title content="MEME title" />
-        <Footer />
+        <Likebutton id="example" />
+        <div id="example"></div>
       </div>
     );
   }
