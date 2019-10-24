dcb58947162dfc977de98d79831aacc53e07ea63
Lydia-coder <lydiatjuh@gmail.com>
2019-10-24 16:29:27 +0200

    new page

--- src/component/Header.js
+++ src/component/Header.js
@@ -5,6 +5,9 @@ export class Header extends React.Component {
     return (
       <div>
         <p>{this.props.name}</p>
+        <div>
+          <h1 style={{ fontSize: 60, color: "white" }}>MEME GENERATOR</h1>
+        </div>
       </div>
     );
   }
