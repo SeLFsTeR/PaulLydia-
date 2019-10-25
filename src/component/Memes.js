9b7a62caaeb74fcecfbb536c124a0062743c9c5e
Lydia-coder <lydiatjuh@gmail.com>
2019-10-25 11:54:10 +0200

    latest version

--- src/component/Memes.js
+++ src/component/Memes.js
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
-// import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-// import Picture from "./Picture";
+import Like from "./Like";
+import { Link } from "react-router-dom";
 
 export class Memes extends Component {
   state = {
     bottomText: "",
-    topText: ""
+    topText: "",
+    src: null
   };
 
   handleChange = event => {
@@ -23,40 +24,45 @@ export class Memes extends Component {
       text0: this.state.topText,
       text1: this.state.bottomText,
       template_id: this.props.id,
-      username: "lydia-coder",
-      password: "Erdfcv00#"
+      username: "SeLFsTeR",
+      password: "lunchbox"
     };
     const url = `https://api.imgflip.com/caption_image?text0=${data.text0}&text1=${data.text1}&template_id=${data.template_id}&username=${data.username}&password=${data.password}`;
-
+    console.log(url);
     fetch(url, {
-      method: "POST", // *GET, POST, PUT, DELETE, etc.
-      mode: "no-cors" // no-cors, *cors, same-origin
-    }).then(response => {
-      console.log(response);
-    });
+      method: "GET", // *GET, POST, PUT, DELETE, etc.
+      mode: "cors" // no-cors, *cors, same-origin
+    })
+      .then(response => {
+        return response.text();
+      })
+      .then(json => {
+        console.log(JSON.parse(json).data.url);
+        this.setState({ src: JSON.parse(json).data.url });
+      });
     this.setState({ bottomText: "", topText: "" });
   };
 
-  // handleClick = () => {
-  //   console.log("hello??");
-  //   this.props.clickImage(this.props.url);
-  // };
   render() {
+    // const { params } = this.props.match;
     return (
       <div className="memeImg">
-        <img
-          // onClick={this.handleClick}
-          style={{
-            width: 800,
-            borderRadius: 2,
-            borderColor: "white",
-            borderStyle: "solid",
-            borderWidth: 6
-          }}
-          src={this.props.url}
-          alt={""}
-        />
+        <Link to={`/memes/${this.props.id}`}>
+          <img
+            // onClick={this.handleClick}
+            style={{
+              width: 800,
+              borderRadius: 2,
+              borderColor: "white",
+              borderStyle: "solid",
+              borderWidth: 6
+            }}
+            src={this.state.src === null ? this.props.url : this.state.src}
+            alt={""}
+          />
+        </Link>
         <p>{this.props.name}</p>
+        <Like />
         <div>
           <form onSubmit={this.handleSubmit}>
             <label>Top Text:</label>
@@ -80,6 +86,3 @@ export class Memes extends Component {
 }
 
 export default Memes;
-{
-  /* <Route path="/Picture" component={Picture} /> */
-}
--- src/component/Picture.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from "react";
-
-export class Picture extends Component {
-  render() {
-    return <div></div>;
-  }
-}
-
-export default Picture;
